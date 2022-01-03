30d937c75195e027c584d6ca8834e1d85f1fb2d8
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-01-04 01:35:28 +0900

    feat: post

---
+++ src/test/java/co/whitetree/springwebflux/WebFlux03PostRequestTest.java
@@ -0,0 +1,30 @@
+package co.whitetree.springwebflux;
+
+import co.whitetree.springwebflux.dto.MathResponse;
+import co.whitetree.springwebflux.dto.MultiplyRequestDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class WebFlux03PostRequestTest extends BaseTest {
+
+    @Autowired
+    WebClient webClient;
+
+    @Test
+    void post() {
+        Mono<MathResponse> responseMono = webClient
+                .post()
+                .uri("reactive-math/multiply")
+                .bodyValue(new MultiplyRequestDto(5, 2))
+                .retrieve()
+                .bodyToMono(MathResponse.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+}
