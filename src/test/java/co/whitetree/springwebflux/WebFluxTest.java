435485f1f286cfcb5dd1076fc6b6aacee525d9b4
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-02-18 22:29:05 +0900

    feat: WebFlux Test

---
+++ src/test/java/co/whitetree/springwebflux/WebFluxTest.java
@@ -0,0 +1,23 @@
+package co.whitetree.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WebFluxTest extends BaseTest {
+
+    @Test
+    void flux() {
+        List<String> items = new ArrayList<>();
+        Flux<String> flux = Flux.just("apple", "amazon")
+                .log();
+        flux.subscribe(items::add);
+
+        assertThat(items).isEqualTo(Arrays.asList("apple", "amazon"));
+    }
+}
